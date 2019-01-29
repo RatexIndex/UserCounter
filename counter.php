6119a73770dd6bab53a5458c8542aea28711ed0d
RatexIndex <13403474+RatexIndex@users.noreply.github.com>
2019-01-29 07:12:54 +0100

    Update counter.php

--- counter.php
+++ counter.php
@@ -1,1 +1,14 @@
-
+<?php
+session_start();
+$counterstand = intval(file_get_contents("count_file.txt"));
+ 
+if(!isset($_SESSION['counter_ip']))
+   {
+   $counterstand++;
+   file_put_contents("static/counter/count_file.txt", $counterstand);
+ 
+   $_SESSION['counter_ip'] = true;
+   }
+ 
+echo $counterstand;
+?>
